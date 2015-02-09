639124dc331bed159e4226be659b201a1fc72568
Richard <richard.clawson@gmail.com>
2015-02-08 22:50:52 -0800

    fixing the dest data load bug

--- TrackerUI/Data/DatabaseDestinationSevice.cs
+++ TrackerUI/Data/DatabaseDestinationSevice.cs
@@ -17,8 +17,8 @@ namespace Tracker.Data
 
         private SQLiteConnection GetConnection()
         {
-             SQLiteConnection connection = new SQLiteConnection(Platform, this.connectionString);
-             return connection;
+            SQLiteConnection connection = new SQLiteConnection(Platform, this.connectionString);
+            return connection;
         }
 
         public DatabaseDestinationSevice(string connString)
@@ -39,15 +39,134 @@ namespace Tracker.Data
 
         public ObservableCollection<Destination> GetDestinations()
         {
-            ObservableCollection<Destination> data;
+            //ObservableCollection<Destination> data;
 
-            using (var connection = GetConnection())
+            //using (var connection = GetConnection())
+            //{
+            //    var dests = connection.Table<Destination>().ToList();
+            //    data = new ObservableCollection<Destination>(dests);
+            //}
+
+            //return data;
+
+            return new ObservableCollection<Destination>(GetHardCoded());
+        }
+
+        private static List<Destination> GetHardCoded()
+        {
+            List<Destination> dests = new List<Destination>();
+            dests.Add(new Destination()
             {
-                var dests = connection.Table<Destination>().ToList();
-                data = new ObservableCollection<Destination>(dests);
-            }
+                DestinationDesc = "Dundas",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "North Mtn",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "South Mtn",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "12 SWS / BMEWS",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "D-Launch",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Back beach / D-launch",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Secret Place / North Mtn",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "ICE wall by BMEWS",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Camp Tuto",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Waterfall/Uvdle",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "P-MTN",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "CE Cabin",
+                RequiresDetail = true
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Mountain Meadow / North Mtn",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "C-Launch",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Other",
+                RequiresDetail = true
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Icecaves towards Cape Atholl",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Cape Atholl",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Green valley",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Station North / RDAF flights",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "CFS Alert / CAF flights",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "NASA flights",
+                RequiresDetail = false
+            });
+            dests.Add(new Destination()
+            {
+                DestinationDesc = "Off defense area",
+                RequiresDetail = true
+            });
 
-            return data;
+            return dests;
         }
     }
 }
